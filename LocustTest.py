721896a46e6817bd1574aa4fa9681659ad270c12
HANZIGUO <hanziguo@kingsoft.com>
2020-01-15 18:29:21 +0800

    'fix'

--- LocustTest.py
+++ LocustTest.py
@@ -1,4 +1,4 @@
-from locust import HttpLocust, TaskSet, task
+from locust import HttpLocust, TaskSet, task, between
 
 
 # 定义用户行为
@@ -19,5 +19,6 @@ class WebsiteUser(HttpLocust):
     # weight = 3
     task_set = UserBehavior
     host = "http://www.baidu.com"
-    min_wait = 3000
-    max_wait = 6000
+    # min_wait = 3000
+    # max_wait = 6000
+    wait_time = between(3, 6)
