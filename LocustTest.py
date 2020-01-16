1c06b9dd600c24beae9facc2f6227747cd83a636
HANZIGUO <hanziguo@kingsoft.com>
2020-01-16 17:28:33 +0800

    'fix'

--- LocustTest.py
+++ LocustTest.py
@@ -6,8 +6,7 @@ class UserBehavior(TaskSet):
 
     @task
     def baidu_index(self):
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
-        response = self.client.get("/", headers=headers, catch_response=True)
+        response = self.client.get("/", catch_response=True)
         # json_text = response.text
         # response = self.client.post("/login", {"username": "testuser", "password": "secret"})
         if response.status_code != 200:
@@ -41,4 +40,4 @@ class WebsiteUserMyweb(HttpLocust):
     # weight = 3
     task_set = UserBehaviorMyWeb
     host = "http://120.92.155.170"
-    wait_time = between(3, 6)
+    wait_time = between(1, 2)
