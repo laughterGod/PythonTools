27dcb9649890825744b1e9bc99a7a7a3a6c130cf
HANZIGUO <hanziguo@kingsoft.com>
2020-01-16 16:46:57 +0800

    'fix'

--- LocustTest.py
+++ LocustTest.py
@@ -19,6 +19,25 @@ class WebsiteUser(HttpLocust):
     # weight = 3
     task_set = UserBehavior
     host = "http://www.baidu.com"
-    # min_wait = 3000
-    # max_wait = 6000
+    wait_time = between(0.001, 0.01)
+
+
+# 定义用户行为
+class UserBehaviorMyWeb(TaskSet):
+
+    @task
+    def myweb_index(self):
+        response = self.client.get("/", catch_response=True)
+        # json_text = response.text
+        # response = self.client.post("/login", {"username": "testuser", "password": "secret"})
+        if response.status_code != 200:
+            response.failure('Failed!')
+        else:
+            response.success()
+
+
+class WebsiteUserMyweb(HttpLocust):
+    # weight = 3
+    task_set = UserBehaviorMyWeb
+    host = "http://120.92.155.170"
     wait_time = between(3, 6)
