28922d280e7b6cba6b61788132ea0f2c46ef7894
HANZIGUO <hanziguo@gmail.com>
2021-05-13 18:23:54 +0800

    fix

--- Tools/AWS.py
+++ Tools/AWS.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- coding:utf-8 -*-
+# coding=utf-8
 
 import sys, os, base64, datetime, hashlib, hmac
 import requests
--- Tools/testaiqiyi.py
+++ Tools/testaiqiyi.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# --*-- coding:utf-8 --*--
+# coding=utf-8
 
 
 def testArr(arr_A, arr_B):
